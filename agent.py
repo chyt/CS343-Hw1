651d27740d9435b54c6b81e3a6b5376c87dd2088
Charles Tian <c.y.t@me.com>
2015-09-12 23:20:18 -0500

    Added iterative deepening

--- agent.py
+++ agent.py
@@ -13,7 +13,9 @@ class IdaStarSearchAgent(SearchAgent):
     IDA* algorithm
     """
 
+    global depth_guess
     global number_moves
+    global current_depth
     def __init__(self):
         """
         A new Agent
@@ -24,13 +26,15 @@ class IdaStarSearchAgent(SearchAgent):
         self.adjlist = {}
         self.parents = {}
         self.number_moves = 0
+        self.current_depth = 0
+        self.depth_guess = manhattan_heuristic(0,0)
+        print self.depth_guess
 
         #self.guess = ROWS + COLS - 2
         #print "guess: %i" % guess
         print "---------------------- IDA* ----------------------"
 
     def idas_action(self, observations):
-        self.number_moves += 1
         r = observations[0]
         c = observations[1]
         current_cell = (r, c)
@@ -50,21 +54,40 @@ class IdaStarSearchAgent(SearchAgent):
         k = 0
         while k < len(adjlist) and adjlist[k] in self.visited:
             k += 1
-        # if we don't have other neighbors to visit, back up
-        if k == len(adjlist):
-            next_cell = self.parents[current_cell]
-        else: # otherwise visit the next place
+
+        # we have arrived at the beginning again
+        if current_cell == self.starting_pos and k == len(adjlist):
+            print "* * * * * * *\ndepth guess of %i failed, returned to begininng\ncurrent depth is %i\nnumber of visited nodes was %i" % (self.depth_guess, self.current_depth, len(self.visited))
+
+            self.visited = set([])
+            #self.parents = {}
+            #self.backpointers = {}
+            #self.number_moves = 0
+
+            self.depth_guess += 1
+            self.current_depth = 0
             next_cell = self.lowest_cost_adj_list(adjlist, k)
+            self.parents[next_cell] = current_cell
+            self.current_depth += 1
+        else:
+            # if we don't have other neighbors to visit, back up
+            if k == len(adjlist) or self.current_depth >= self.depth_guess:
+                next_cell = self.parents[current_cell]
+                self.current_depth -= 1
+            else: # otherwise visit the next place
+                next_cell = self.lowest_cost_adj_list(adjlist, k)
+                self.current_depth += 1
 
         self.visited.add(current_cell) # add this location to visited list
         if current_cell != self.starting_pos:
-            get_environment().mark_maze_blue(r, c) # mark it as blue on the maze
+            get_environment().mark_maze_yellow(r, c) # mark it as yellow on the maze
         v = self.constraints.get_instance() # make the action vector to return
         dr, dc = next_cell[0] - r, next_cell[1] - c # the move we want to make
         v[0] = get_action_index((dr, dc))
         # remember how to get back
         if next_cell not in self.backpointers:
             self.backpointers[next_cell] = current_cell
+        self.number_moves += 1
         return v
 
     def lowest_cost_adj_list(self, adjlist, k):
@@ -96,6 +119,7 @@ class IdaStarSearchAgent(SearchAgent):
         self.parents = {}
         self.backpointers = {}
         self.starting_pos = None
+        self.number_moves = 0
 
     def act(self, time, observations, reward):
         # return action
@@ -104,6 +128,7 @@ class IdaStarSearchAgent(SearchAgent):
     def end(self, time, reward):
         print "Total number of moves: %i" % self.number_moves
         print "Final reward: %f, cumulative: %f" % (reward[0], self.fitness[0])
+        print "resetting"
         self.reset()
         return True
 
